63042723ceaf1d088bc9aca198d10a673afdb186
dipta harimbawa <vipalkuda@gmail.com>
2022-08-26 22:33:23 +0700

    [fix] image

--- pages/address/index.vue
+++ pages/address/index.vue
@@ -4,6 +4,7 @@
         <div class="text-header my-4">Alamat</div>
     </div>
     <div class="col-12 align-self-start text-center p-0">
+      <img class="mb-1" :src="require(`~/assets/images/Empty State - Address.png`)">
       <p class="address-bold-title">Belum ada Alamat terdaftar nih</p>
       <p class="address-regular-desc">Klik "Tambah Alamat" untuk menambahkan Alamat pengiriman dan mulai pesananmu</p>
       <b-button :to="{path: '/address/add'}" class="address-btn" variant="outline-primary"><span class="address-text-btn">Tambah Alamat</span></b-button>
